1e595e291e7a54cb741fd971cc5202f2004a390d
ivanavocode <ivanavocode@gmail.com>
2019-02-08 21:58:24 +0600

    add Webpack
    webpack+babel

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import './js/common'
+
+
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
+
+// import 'vue'
+// import Vue from 'vue'
+// Vue.use()
+window.Vue = require('vue')
